81337e04f0289ea40e791f22ab9961cd32704053
shuaib shaban <shuaib>
2023-01-23 11:08:51 +0300

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,14 +1,10 @@
-import React from "react";
 
-function QuestionItem({ questions, question, onDelete, onAnswerChange }) {
+import React, { useState } from "react";
+
+function QuestionItem({ question, onDeleteItem , onUpdateItem }) {
   const { id, prompt, answers, correctIndex } = question;
+  const [initialQuestion, setQuestion] =useState(question)
 
-  function deleteQuestion() {
-    onDelete(id);
-  }
-  function answerChange(event) {
-    onAnswerChange(id, parseInt(event.target.value));
-  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -16,17 +12,38 @@ function QuestionItem({ questions, question, onDelete, onAnswerChange }) {
     </option>
   ));
 
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${initialQuestion.id}`, {
+      method:'DELETE'
+    })
+    .then(res => res.json())
+    .then(() => onDeleteItem(question))
+    .catch((error) => console.log(error))
+  }
+
+  function handleChangeAnswer(event) {
+    const value = event.target.value;
+    setQuestion({...initialQuestion, correctIndex:value})
+    console.log(initialQuestion)
+
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method:'PATCH',
+      headers: {'Content-Type':'application/json'},
+      body:JSON.stringify({correctIndex:value})
+    })
+    .then(res => res.json())
+    .then(updatedItem => onUpdateItem(updatedItem))
+    .catch(error => console.log(error));
+  }
   return (
     <li>
-      <h4>Question {questions.indexOf(question) + 1}</h4>
+      <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex} onChange={answerChange}>
-          {options}
-        </select>
+        <select defaultValue={correctIndex} onChange={handleChangeAnswer}>{options}</select>
       </label>
-      <button onClick={deleteQuestion}>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
